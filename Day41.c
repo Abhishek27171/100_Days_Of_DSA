bd415fc9de8a94c8f5b35c65691f8a5116fcbeac
Abhishek27171 <aabhiisheek111@gmail.com>
2026-04-04 21:51:23 +0530

    day41 code

---
+++ Day41.c
@@ -0,0 +1,89 @@
+/*Problem Statement:
+Implement a Queue using a linked list supporting enqueue and dequeue operations.
+
+Input Format:
+- First line contains integer N
+- Next N lines contain queue operations
+
+Output Format:
+- Print dequeued elements
+- Print -1 if dequeue is attempted on an empty queue*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Node structure
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *front = NULL;
+struct Node *rear = NULL;
+
+// Enqueue operation
+void enqueue(int value)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->next = NULL;
+
+    // if queue is empty
+    if (rear == NULL)
+    {
+        front = rear = newNode;
+        return;
+    }
+
+    rear->next = newNode;
+    rear = newNode;
+}
+
+// Dequeue operation
+void dequeue()
+{
+    if (front == NULL)
+    {
+        printf("-1\n");
+        return;
+    }
+
+    struct Node *temp = front;
+    printf("%d\n", temp->data);
+
+    front = front->next;
+
+    // if queue becomes empty
+    if (front == NULL)
+        rear = NULL;
+
+    free(temp);
+}
+
+int main()
+{
+    int N, value;
+    char operation[20];
+
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++)
+    {
+
+        scanf("%s", operation);
+
+        if (strcmp(operation, "enqueue") == 0)
+        {
+            scanf("%d", &value);
+            enqueue(value);
+        }
+        else if (strcmp(operation, "dequeue") == 0)
+        {
+            dequeue();
+        }
+    }
+
+    return 0;
+}
